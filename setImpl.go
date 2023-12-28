9e7dc6a495a9ada06e45b73bffc463583621dd0b
Thomas <thomas.sarpong@yum.com>
2023-12-28 00:48:51 -0500

    lint

--- setImpl.go
+++ setImpl.go
@@ -1,7 +1,6 @@
 package set
 
 import (
-	"fmt"
 	"sync"
 )
 
@@ -37,6 +36,7 @@ func (s *setImpl) addInternal(item any) {
 func (s *setImpl) Add(item any) {
 	s.mut.Lock()
 	defer s.mut.Unlock()
+
 	s.addInternal(item)
 }
 
@@ -47,12 +47,14 @@ func (s *setImpl) hasInternal(item any) bool {
 		return false
 	}
 	itemAccess := s.byAccess[index]
+
 	return itemAccess == item
 }
 
 func (s *setImpl) Has(item any) bool {
 	s.mut.Lock()
 	defer s.mut.Unlock()
+
 	return s.hasInternal(item)
 }
 
@@ -61,23 +63,28 @@ func (s *setImpl) Contains(set *setImpl) bool {
 	if set.lenInternal() > s.lenInternal() {
 		return false
 	}
+
 	s.mut.Lock()
 	defer s.mut.Unlock()
+
 	for _, item := range set.toSliceInternal() {
 		if exists := s.hasInternal(item); !exists {
 			return false
 		}
 	}
+
 	return true
 }
 
 func (s *setImpl) Remove(item any) {
 	s.mut.Lock()
 	defer s.mut.Unlock()
+
 	index, ok := s.byKey[item]
 	if !ok {
 		return
 	}
+
 	delete(s.byKey, item)
 	s.byAccess = append(s.byAccess[:index], s.byAccess[index+1:]...)
 }
@@ -86,6 +93,7 @@ func (s *setImpl) Len() int {
 	s.mut.Lock()
 	defer s.mut.Unlock()
 	length := s.lenInternal()
+
 	return length
 }
 func (s *setImpl) lenInternal() int {
@@ -99,13 +107,16 @@ func (s *setImpl) IsEmpty() bool {
 func (s *setImpl) toSliceInternal() []any {
 	slice := make([]any, s.lenInternal())
 	copy(slice, s.byAccess)
+
 	return slice
 }
 
 func (s *setImpl) ToSlice() []any {
 	s.mut.Lock()
 	defer s.mut.Unlock()
+
 	slice := s.toSliceInternal()
+
 	return slice
 }
 
@@ -121,6 +132,7 @@ func (s *setImpl) Union(set *setImpl) *setImpl {
 
 	go func() {
 		defer wg.Done()
+
 		for _, item := range s.toSliceInternal() {
 			newSet.addInternal(item)
 		}
@@ -128,6 +140,7 @@ func (s *setImpl) Union(set *setImpl) *setImpl {
 
 	go func() {
 		defer wg.Done()
+
 		for _, item := range set.toSliceInternal() {
 			newSet.addInternal(item)
 		}
@@ -152,6 +165,7 @@ func (s *setImpl) Intersection(set *setImpl) *setImpl {
 			results.addInternal(item)
 		}
 	}
+
 	return results
 }
 
@@ -169,6 +183,7 @@ func (s *setImpl) Complement(set *setImpl) *setImpl {
 			results.addInternal(item)
 		}
 	}
+
 	return results
 }
 
@@ -176,6 +191,7 @@ func (s *setImpl) Iterator() *iteratorImpl {
 	itr := newIterator(s)
 	s.mut.Lock()
 	itr.prepareNext()
+
 	return itr
 }
 
@@ -194,7 +210,6 @@ func (itr *iteratorImpl) prepareNext() {
 		if index+1 >= itr.set.lenInternal() {
 			itr.nextItem = nil
 		} else {
-			fmt.Println(index)
 			itr.nextItem = &itr.set.byAccess[index+1]
 		}
 	}
@@ -208,8 +223,10 @@ func (itr *iteratorImpl) Next() any {
 	if itr.nextItem == nil {
 		panic("Set out of range")
 	}
+
 	next := itr.nextItem
 	itr.prepareNext()
+
 	return next
 }
 
